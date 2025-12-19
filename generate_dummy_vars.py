b6d10f30ee408ecb193bc9ee018daf73df2c3e71
Emanuele Di Marco <emanuele.dimarco@gmail.com>
2025-12-19 15:21:46 +0100

    make the steps fixed

--- generate_dummy_vars.py
+++ generate_dummy_vars.py
@@ -1,55 +1,57 @@
 import ROOT
 import numpy as np
 
-def generate_toys(Npairs, output_file, N=int(1e4)):
+def generate_toys(Npoints, output_file, N=int(1e4)):
 
     f = ROOT.TFile(output_file, "RECREATE")
     h2 = ROOT.TH2D("h2","dummy 2D vars", 100, -3, 7, 100, -5, 5)
 
     # variabili indipendenti
-    x = np.random.uniform(-1, 1, Npairs)
-    y = np.random.uniform(-1, 1, Npairs)
+    xarr = np.linspace(-1, 1, Npoints)
+    yarr = np.linspace(-1, 1, Npoints)
 
     histos = []
 
-    for ip in range(Npairs):
-        histos.append(h2.Clone(f"h2_{ip}"))
+    for ix,x in enumerate(xarr):
+        for iy,y in enumerate(yarr):
+            histos.append(h2.Clone(f"h2_{ix}_{iy}"))
+            histos[-1].SetTitle(f"(x,y) = ({x:.2f},{y:.2f})")
+            
+            # -------------------------
+            # Medie (polinomi in x,y)
+            # -------------------------
+            mu1 =  2.0 + 0.5*x - 0.3*y + 0.1*x*y
+            mu2 = -1.0 + 0.4*x + 0.6*y
+            
+            # -------------------------
+            # RMS (polinomi in x,y)
+            # -------------------------
+            sigma1 = 1.0 + 0.2*x**2 + 0.1*y**2
+            sigma2 = 0.8 + 0.1*x**2 + 0.1*y**2
+            
+            # -------------------------
+            # Correlazione
+            # -------------------------
+            rho = 0.6
+            
+            # Gaussiane standard indipendenti
+            print(f"Generating two Gaussians with {N} events for pair ({x:.2f},{y:.2f})...")
+            z1 = np.random.normal(size=N)
+            z2 = np.random.normal(size=N)
+            
+            # -------------------------
+            # Loop Monte Carlo + Fill
+            # -------------------------
+            print(f"Monte Carlo generation of 2D correlated distribution...")
+            for i in range(N):
+                if i%5000 == 0:
+                    print(f"\tSampling the Gaussians for event {i}")
+                g1 = mu1 + sigma1 * z1[i]
+                g2 = mu2 + sigma2 * (rho * z1[i] + np.sqrt(1 - rho**2) * z2[i])
+                histos[-1].Fill(g1, g2)
 
-        # -------------------------
-        # Medie (polinomi in x,y)
-        # -------------------------
-        mu1 =  2.0 + 0.5*x[ip] - 0.3*y[ip] + 0.1*x[ip]*y[ip]
-        mu2 = -1.0 + 0.4*x[ip] + 0.6*y[ip]
-
-        # -------------------------
-        # RMS (polinomi in x,y)
-        # -------------------------
-        sigma1 = 1.0 + 0.2*x[ip]**2 + 0.1*y[ip]**2
-        sigma2 = 0.8 + 0.1*x[ip]**2 + 0.1*y[ip]**2
-
-        # -------------------------
-        # Correlazione
-        # -------------------------
-        rho = 0.6
-
-        # Gaussiane standard indipendenti
-        print(f"Generating two Gaussians with {N} events for pair ({x[ip]:.2f},{y[ip]:.2f})...")
-        z1 = np.random.normal(size=N)
-        z2 = np.random.normal(size=N)
-
-        # -------------------------
-        # Loop Monte Carlo + Fill
-        # -------------------------
-        print(f"Monte Carlo generation of 2D correlated distribution...")
-        for i in range(N):
-            if i%5000 == 0:
-                print(f"\tSampling the Gaussians for event {i}")
-            g1 = mu1 + sigma1 * z1[i]
-            g2 = mu2 + sigma2 * (rho * z1[i] + np.sqrt(1 - rho**2) * z2[i])
-            histos[ip].Fill(g1, g2)
-
-        f.cd()
-        histos[ip].Write()
+            f.cd()
+            histos[-1].Write()
 
     f.Close()
 
@@ -59,7 +61,7 @@ if __name__ == "__main__":
 
     toy_sim = "toy_simulation.root"
     print(f"Simulate the 'simulation' and store in the file {toy_sim}")
-    generate_toys(121,toy_sim)
+    generate_toys(11,toy_sim)
 
     toy_data = "toy_data.root"
     print(f"Simulate the 'data' and store in the file {toy_data}")
